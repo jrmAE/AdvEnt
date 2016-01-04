5ebd9ea3708839f39fd1485aa95a7755eaae1c2a
meyer <joseph.r.meyer@gmail.com>
2016-01-03 22:34:49 -0500

    Initial commit

---
+++ src/ae/raze/RazeApplication.java
@@ -0,0 +1,41 @@
+/**
+ * AE application RAZE
+ */
+package ae.raze;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ * @author meyer
+ */
+public class RazeApplication extends SimpleApplication {
+
+	/* (non-Javadoc)
+	 * @see com.jme3.app.SimpleApplication#simpleInitApp()
+	 */
+	@Override
+	public void simpleInitApp() {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		RazeApplication app = new RazeApplication();
+        app.start(); // start the game
+
+	}
+
+}
