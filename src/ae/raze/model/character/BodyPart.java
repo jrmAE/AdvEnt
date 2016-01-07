d1bb06b7c26d515f8c8bf420e3753fd1548c6ead
meyer <joseph.r.meyer@gmail.com>
2016-01-06 22:13:09 -0500

    abstract parent

---
+++ src/ae/raze/model/character/BodyPart.java
@@ -0,0 +1,42 @@
+/**
+ * AE Application RAZE
+ * BodyPart.java
+ */
+package ae.raze.model.character;
+
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+
+import ae.raze.util.ApplicationManager;
+
+/**
+ * @author meyer
+ *
+ */
+public abstract class BodyPart {
+
+	protected Geometry geom;
+
+	protected Material getDefaultMaterial(ColorRGBA color) {
+		Material mat = new Material(ApplicationManager.INSTANCE.getAssetManager(),"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", color);   // set color of material to blue
+		return mat;
+	}
+	
+	/**
+	 * @return the geom
+	 */
+	public Geometry getGeom() {
+		return geom;
+	}
+
+	/**
+	 * @param geom the geom to set
+	 */
+	public void setGeom(Geometry geom) {
+		this.geom = geom;
+	}
+	
+	
+}
