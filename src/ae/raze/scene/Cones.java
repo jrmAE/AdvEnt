bd053627e7bb6d0058bff12496afbcdc9c06cd00
meyer <joseph.r.meyer@gmail.com>
2016-06-16 22:27:26 -0400

    Added to expand cone options

---
+++ src/ae/raze/scene/Cones.java
@@ -0,0 +1,43 @@
+/**
+ * AE Application RAZE
+ * Cones.java
+ */
+package ae.raze.scene;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Dome;
+
+/**
+ * Provides common methods to generate 'cones' that creates a track for
+ * the cars to follow
+ * 
+ * @author meyer
+ *
+ */
+public class Cones {
+	
+	public static void addCones(Node rootNode, PhysicsSpace space, AssetManager assetManager) {
+		// road cones
+		for (int i = 0; i < 12; i++) { 
+			Vector3f v3f = new Vector3f(0, 0, 0);
+			Dome dome = new Dome(v3f, 2, 4, .75f, true);
+			Geometry domeGeometry = new Geometry("Dome", dome); 
+			Material coneMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+			coneMaterial.setColor("Color", ColorRGBA.Orange);
+			domeGeometry.setMaterial(coneMaterial); 
+			domeGeometry.setLocalTranslation(i, 5, -3); 
+			// RigidBodyControl automatically uses box collision shapes when attached to single geometry with box mesh 
+			domeGeometry.addControl(new RigidBodyControl(2)); 
+			rootNode.attachChild(domeGeometry); 
+			space.add(domeGeometry); 
+		} 
+	}
+
+}
