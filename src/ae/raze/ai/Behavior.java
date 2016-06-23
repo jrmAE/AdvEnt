41a4afa0db1a6c1aa545514769effdffb887bc38
meyer <joseph.r.meyer@gmail.com>
2016-06-22 22:29:29 -0400

    created

---
+++ src/ae/raze/ai/Behavior.java
@@ -0,0 +1,15 @@
+/**
+ * AE Application RAZE
+ * Behavior.java
+ */
+package ae.raze.ai;
+
+/**
+ * TODO beginning work for the AI for the CPU cars
+ * 
+ * @author meyer
+ *
+ */
+public class Behavior {
+
+}
