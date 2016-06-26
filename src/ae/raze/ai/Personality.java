c91f67d2402945e7fd3b1c836161c40c3b1827f0
meyer <joseph.r.meyer@gmail.com>
2016-06-25 21:40:37 -0400

    Created

---
+++ src/ae/raze/ai/Personality.java
@@ -0,0 +1,15 @@
+/**
+ * AE Application RAZE
+ * Personality.java
+ */
+package ae.raze.ai;
+
+/**
+ * The 'personality' of the CPU cars
+ * 
+ * @author meyer
+ */
+public enum Personality {
+	AGGRESIVE,
+	PASSIVE;
+}
